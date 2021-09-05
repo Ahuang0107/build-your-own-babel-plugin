ecee87a8996f366a91800e08a3ec7918eb3c8c7e
Elase Huang <seto1920748235@126.com>
2021-09-05 11:35:27 +0800

    init

---
+++ test/index.jsx
@@ -0,0 +1,9 @@
+import React from "./react";
+import ReactDOM from './react-dom.js';
+import App from "./App.jsx";
+
+/** @jsx React.createElement */
+ReactDOM.render(
+    <App name={'Elase'}/>,
+    document.getElementById("root")
+)
